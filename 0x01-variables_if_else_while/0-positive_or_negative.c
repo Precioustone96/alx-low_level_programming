58d70f5dd0aa9fb1c0ba87fabaa1ddcff7923c6e
precioustone96 <Precioustone96@gmail.com>
2022-10-14 21:02:31 -0700

    no 0 corrected 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
 /**
- * main - my function for randon number assignment to n starts here
- * Return: - expected output is 0
+ * main - start point
+ * Return: gives  0 as  correct output
  */
-int main(void)
-{
+	int main(void)
+	{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (n == 0)
 	{
-	printf("%d is positive\n", n);
+	printf("%i is zero\n", n);
 	}
-	else if  (n == 0)
+
+	else if (n < 0)
 	{
-	printf("%d is zero\n")
+
+	printf("%i is negative\n", n);
+
 	}
+
 	else
 	{
-	printf("%d is negative\n")
+	printf("%i is positive\n", n);
 	}
 	return (0);
+	}
 }
