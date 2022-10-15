67455a467203e692671ae4b32e4272795c68f928
precioustone96 <Precioustone96@gmail.com>
2022-10-15 10:43:09 -0700

    no 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of main function
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+{
+	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+
+}
