0de1012d4341bcf69724111ab22828d67a0d8a1b
precioustone96 <Precioustone96@gmail.com>
2022-10-15 19:50:01 -0700

    no 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+/**
+ * main - point of Entry for my code
+ * Return: output a 0 when successful
+ */
+
+int main(void)
+{
+	int z;
+
+for (z = 48; z < 58; z++)
+{
+putchar(z);
+}
+for (z = 'a'; z <= 'f'; z++)
+{
+putchar(z);
+}
+putchar('\n');
+return (0);
+}
