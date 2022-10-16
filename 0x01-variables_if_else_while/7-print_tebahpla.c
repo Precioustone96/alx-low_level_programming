89a7b9faea24c380b6eeb0a0e73082f72de31e4b
precioustone96 <Precioustone96@gmail.com>
2022-10-15 18:59:44 -0700

    no 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrry point of my code
+ * Return: give a value of 0 after my code is done
+ */
+
+int main(void)
+
+{
+	char boy = 'z';
+
+	while (boy >= 'a')
+{
+	putchar(boy);
+	boy--;
+}
+	putchar('\n');
+	return (0);
+	}
